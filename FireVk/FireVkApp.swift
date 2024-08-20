60808e5ecccf021756b1d8fbab4778a67bfc563f
danilacrazych <danilacrazych@mail.ru>
2024-08-20 22:17:12 +0300

    Initial Commit

---
+++ FireVk/FireVkApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireVkApp.swift
+//  FireVk
+//
+//  Created by Данил Уваров on 20.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct FireVkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
